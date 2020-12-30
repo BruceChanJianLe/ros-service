75575c54dd3a11554d9058d22fa18779c8545f53
brucechanjianle <jianle001@e.ntu.edu.sg>
2020-12-30 09:35:29 +0800

    style: update with comment

--- src/ros_service_node.cpp
+++ src/ros_service_node.cpp
@@ -3,10 +3,13 @@
 
 int main(int argc, char ** argv)
 {
+    // Initialize ROS node
     ros::init(argc, argv, "basic_service");
 
+    // Instantiate ros_service obj
     basic_service::ros_service node;
 
+    // Run
     node.run();
 
     return 0;
