4f01c6b2bd5398f71931a63f48afdc2664b3dc7a
brucechanjianle <jianle001@e.ntu.edu.sg>
2020-12-30 09:36:10 +0800

    add service client source code

---
+++ src/ros_service_client.cpp
@@ -0,0 +1,42 @@
+#include <ros_service/ros_service_client.hpp>
+
+
+namespace basic_service
+{
+    ros_service_client::ros_service_client()
+    :   private_nh_(ros::NodeHandle("~")),
+        global_nh_(ros::NodeHandle()),
+        srv_client_(global_nh_.serviceClient<ros_service::Service>("basic_service/basic_service"))
+    {
+    }
+
+
+    ros_service_client::~ros_service_client()
+    {
+    }
+
+
+    void ros_service_client::run()
+    {
+        // Set service request content
+        srv_msg_.request.enabled = true;
+
+        if(srv_client_.call(srv_msg_))
+        {
+            ROS_INFO_STREAM(
+                ros::this_node::getName() <<
+                " successfully enabled service client: " <<
+                srv_msg_.response.done
+            );
+        }
+        else
+        {
+            ROS_INFO_STREAM(
+                ros::this_node::getName() <<
+                " failed to enable service client: " <<
+                srv_msg_.response.done
+            );
+        }
+
+    }
+} // namespace basic_service
